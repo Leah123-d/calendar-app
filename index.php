387cf31d2d8b1bbe738756e6e61468c0ca33480d
Leah123-d <95731243+Leah123-d@users.noreply.github.com>
2025-07-13 19:27:13 -0700

    adds meta data

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <!DOCTYPE html>
+<html lang="en" dir="ltr">
+  
+  <head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="wideth=device-width, initial-scale=1.0" />
+    <title> Calendar App </title>
+
+    <meta name="description" content="My Calendar App 2025"/>
+
+    <link rel="stylesheet" href="">
+  </head>
+
+
+</html>
